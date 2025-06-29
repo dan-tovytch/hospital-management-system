3fdd9dcca7ef0a9d0640c1fef0c5960f02a590f3
dan-tovytch <danielmelentovytch@gmail.com>
2025-06-29 16:26:10 -0300

    feat: adiciona política de autorização para gerenciamento de enfermeiros

---
+++ app/Policies/NursePolicy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class NursePolicy
+{
+    // policy: somente usuários com profile_id 3 podem gerenciar qualquer registro de enfermeiro
+    public function manage(User $user)
+    {
+        return $user->profile_id == 3;
+    }
+}
