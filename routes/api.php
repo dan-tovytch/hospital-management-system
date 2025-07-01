12df7ba3cdee2f0a9ed24a93f9ad4709b10e0a0b
dan-tovytch <danielmelentovytch@gmail.com>
2025-06-30 21:37:02 -0300

    feat: adiciona rotas para criação de agenda na API

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NursesController;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\QueriesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,17 +14,27 @@ Route::post('/register', [AuthController::class, 'register'])->name('user.regist
 Route::post('/login', [AuthController::class, 'login'])->name('user.login');
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('user')->group(function () {
+        Route::put('update/password', [UserController::class, 'updatePassword'])->name("user.updated.password");
+    });
+
     Route::prefix('address')->group(function () {
         Route::post("register", [AddressController::class, 'store'])->name('address.register');
         Route::put("update", [AddressController::class, 'update'])->name('address.update');
     });
+
     Route::prefix("patients")->group(function () {
         Route::post("register", [PatientController::class, 'store'])->name("patients.register");
         Route::put("update", [PatientController::class, 'update'])->name("patients.update");
     });
+
     Route::prefix("nurses")->group(function () {
         Route::post("register", [NursesController::class, 'store'])->name("nurse.register");
         Route::put("update", [NursesController::class, 'update'])->name("nurse.update");
         Route::put("disable", [NursesController::class, 'disable']);
+    });
+
+    Route::prefix("agenda")->group(function () {
+        Route::post("create", [AgendaController::class, 'dailyAgenda'])->name("queries.create");
     });
 });
