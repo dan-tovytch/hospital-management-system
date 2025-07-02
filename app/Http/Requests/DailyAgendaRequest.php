2ddf42bc5787ecd45830ea58b198cab67b5a15b3
dan-tovytch <danielmelentovytch@gmail.com>
2025-07-01 22:30:49 -0300

    feat: adiciona validação e mensagens de erro para o campo 'id' e 'active' na requisição da agenda diária

--- app/Http/Requests/DailyAgendaRequest.php
+++ app/Http/Requests/DailyAgendaRequest.php
@@ -23,9 +23,30 @@ class DailyAgendaRequest extends FormRequest
     {
         return [
             "days_week" => "required|array",
+            "days_week.*.id" => "integer",
             "days_week.*.day" => "required|integer|min:1|max:7",
             "days_week.*.start" => "required|date_format:H:i",
             'days_week.*.end'   => 'required|date_format:H:i|after:days_week.*.start',
+            'days_week.*.active' => "boolean",
+        ];
+        }
+
+        public function messages(): array
+        {
+        return [
+            'days_week.required' => 'O campo dias da semana é obrigatório.',
+            'days_week.array' => 'O campo dias da semana deve ser enviado em um array.',
+            'days_week.*.id.integer' => 'O ID do dia da semana deve ser um número inteiro.',
+            'days_week.*.day.required' => 'O campo dia é obrigatório.',
+            'days_week.*.day.integer' => 'O campo dia deve ser um número inteiro.',
+            'days_week.*.day.min' => 'O campo dia deve ser no mínimo 1.',
+            'days_week.*.day.max' => 'O campo dia deve ser no máximo 7.',
+            'days_week.*.start.required' => 'O campo horário início é obrigatório.',
+            'days_week.*.start.date_format' => 'O campo horário início deve estar no formato HH:MM.',
+            'days_week.*.end.required' => 'O campo horário fim é obrigatório.',
+            'days_week.*.end.date_format' => 'O campo horário fim deve estar no formato HH:MM.',
+            'days_week.*.end.after' => 'O campo fim deve ser uma hora após o início.',
+            'days_week.*.active.boolean' => 'O campo ativo deve ser verdadeiro ou falso.',
         ];
     }
 }
